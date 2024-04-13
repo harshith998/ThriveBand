fb4702ed2699ffe51e24bdd35705a3e02e85082a
chandrasuda <kiran1234c@gmail.com>
2024-04-12 22:47:33 -0500

    init

--- src/components/hero-banner.js
+++ src/components/hero-banner.js
@@ -1,17 +1,16 @@
 import React from "react";
 
 export const HeroBanner = () => {
-  const logo = "https://cdn.auth0.com/blog/developer-hub/react-logo.svg";
+  const logo = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2Fthrive-logo-png-logo-vector-brand-downloads-svg-eps--651473902369552850%2F&psig=AOvVaw3KKQV4S9woN_yg54-3IGEX&ust=1713066434461000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKDB6POjvoUDFQAAAAAdAAAAABAE";
 
   return (
     <div className="hero-banner hero-banner--pink-yellow">
       <div className="hero-banner__logo">
         <img className="hero-banner__image" src={logo} alt="React logo" />
       </div>
-      <h1 className="hero-banner__headline">Hello, React World!</h1>
+      <h1 className="hero-banner__headline">Thrive Band</h1>
       <p className="hero-banner__description">
-        This is a sample application that demonstrates the authentication flow
-        for React apps using <strong>Auth0</strong>.
+      A holistic wearable for Tracking Health, Regulating Intensity, and Verifiying Enudurance (THRIVE). Our device is a wrist device containing numerous sensors, allowing for a complete workout and fitness supplement.
       </p>
       <a
         id="code-sample-link"
@@ -20,7 +19,7 @@ export const HeroBanner = () => {
         href="https://auth0.com/developers/hub/code-samples/spa/react-javascript/basic-authentication-with-react-router-6"
         className="button button--secondary"
       >
-        Check out the React code sample →
+        Check out Thrive Band in action →
       </a>
     </div>
   );
