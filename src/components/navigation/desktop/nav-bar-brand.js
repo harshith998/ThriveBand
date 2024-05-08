ee301ae670804408c4468470bd4ccf06359d57b2
chandrasuda <kiran1234c@gmail.com>
2024-05-08 18:22:43 -0500

    init

--- src/components/navigation/desktop/nav-bar-brand.js
+++ src/components/navigation/desktop/nav-bar-brand.js
@@ -5,13 +5,6 @@ export const NavBarBrand = () => {
   return (
     <div className="nav-bar__brand">
       <NavLink to="/">
-        <img
-          className="nav-bar__logo"
-          src="https://i.pinimg.com/originals/a9/0e/c5/a90ec5e3493a27f243d10fab79d00f28.png"
-          alt="Thrive Band shield logo"
-          width="90"
-          height="49"
-        />
       </NavLink>
     </div>
   );
